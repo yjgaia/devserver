960156710b136528e78f6f6c25fde04322c5ea93
yjgaia <yjdev88@gmail.com>
2024-10-22 12:33:56 +0900

    Update package.json with new dependencies

--- src/run.ts
+++ src/run.ts
@@ -1,6 +1,14 @@
+import { program } from "commander";
 import Config from "./Config.js";
 import FileServer from "./FileServer.js";
 
 export default async function run(config: Config) {
+  const options = program.option(
+    "-p, --port <port>",
+    "Port to listen on",
+    `${config.webServerPort}`,
+  ).parse().opts<{ port: string }>();
+
+  config.webServerPort = parseInt(options.port);
   new FileServer(config);
 }
